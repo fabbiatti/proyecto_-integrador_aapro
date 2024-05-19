928a322db91c246e08bede349acec3d89ecfe515
Facundo Abbiatti <fabbiatti@gmail.com>
2024-05-19 18:31:31 -0300

    Tarea clase 10

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nombre="Juan";
+let apellido="Perez";
+console.log(nombre+" "+apellido);
+let numeroUno=30;
+let numeroDos=35;
+let suma=numeroUno + numeroDos;
+console.log(numeroUno+" + "+numeroDos+": "+suma);
